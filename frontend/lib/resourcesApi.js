48af7011ae7ceb0ebfb944c5ceef670281027caa
shroommu <alexa.kruckenberg@gmail.com>
2026-05-08 22:01:31 -0600

    remove hardcoded localhost

--- frontend/lib/resourcesApi.js
+++ frontend/lib/resourcesApi.js
@@ -1,5 +1,4 @@
-const DEFAULT_API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+const DEFAULT_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
 
 function normalizeBaseUrl(baseUrl) {
   return (baseUrl || "").replace(/\/$/, "");
