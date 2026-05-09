4799de46482250e887f2bbca6b9ade97db380b49
shroommu <alexa.kruckenberg@gmail.com>
2026-05-08 20:47:20 -0600

    background change

--- frontend/components/LeafletClusterMap.jsx
+++ frontend/components/LeafletClusterMap.jsx
@@ -110,8 +110,8 @@ export default function LeafletClusterMap({
           style={{ height: "100%", width: "100%" }}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           />
 
           <MarkerClusterGroup
